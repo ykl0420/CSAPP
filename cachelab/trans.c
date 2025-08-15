57ba89721c84b8d1eef1919bd8ead38c6bb5ddc9
ykl <2841688659@qq.com>
2025-08-16 03:46:45 +0800

    done with cachelab

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -35,7 +35,76 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
 			}
 		}
 	}else if(N == 64){
-		
+		// freopen("out.txt","w",stdout);
+		// printf("NASDA %ld\n",(int*)B - (int*)A);
+		int i,j,p,q,u,tmp;
+		for(i = 0; i < N; i += 8){
+			for(j = 0; j < M; j += 8){
+				if(i == j || i == 0 || j == 0) continue;
+				u = 0;
+				for(p = 0; p < 8; p ++){
+					for(q = 0; q < 4; q ++) B[q + j][p + i] = A[p + i][q + j];
+					for(q = 4; q < 8; q ++) B[q - 4 + u][p + u] = A[p + i][q + j];
+				}
+				for(q = 4; q < 8; q ++) for(p = 0; p < 8; p ++)	B[q + j][p + i] = B[q - 4 + u][p + u];
+			}
+		}
+		for(i = 0; i < N; i += 8){
+			for(j = 0; j < M; j += 8){
+				if(i == j || (i != 0 && j != 0) || i == 48 || j == 48) continue;
+				u = 48;
+				for(p = 0; p < 8; p ++){
+					for(q = 0; q < 4; q ++) B[q + j][p + i] = A[p + i][q + j];
+					for(q = 4; q < 8; q ++) B[q - 4 + u][p + u] = A[p + i][q + j];
+				}
+				for(q = 4; q < 8; q ++) for(p = 0; p < 8; p ++)	B[q + j][p + i] = B[q - 4 + u][p + u];
+			}
+		}
+		for(i = 0; i < N; i += 8){
+			for(j = 0; j < M; j += 8){
+				if(!(i == 0 && j == 48) && !(i == 48 && j == 0)) continue;
+				u = 56;
+				for(p = 0; p < 8; p ++){
+					for(q = 0; q < 4; q ++) B[q + j][p + i] = A[p + i][q + j];
+					for(q = 4; q < 8; q ++) B[q - 4 + u][p + u] = A[p + i][q + j];
+				}
+				for(q = 4; q < 8; q ++) for(p = 0; p < 8; p ++)	B[q + j][p + i] = B[q - 4 + u][p + u];
+			}
+		}
+		for(i = 0; i < N; i += 8){
+			if(i == 56) continue;
+			u = 56,j = i;
+			for(p = 0; p < 8; p ++){
+				for(q = 0; q < 4; q ++){
+					if(p == q || p == q + 4) tmp = A[p + i][q + j];
+					else B[q + j][p + i] = A[p + i][q + j];
+				}
+				for(q = 4; q < 8; q ++) B[q - 4 + u][p + u] = A[p + i][q + j];
+				if(p < 4) B[p + j][p + i] = tmp;
+				else B[p - 4 + j][p + i] = tmp;
+			}
+			for(q = 4; q < 8; q ++) for(p = 0; p < 8; p ++)	B[q + j][p + i] = B[q - 4 + u][p + u];
+		}
+		i = 56,j = 56;
+		for(p = 0; p < 8; p ++){
+			for(q = 0; q < 8; q ++){
+				B[q + j][p + i] = A[p + i][q + j];
+			}
+		}
+	}else{
+		for(int i = 0; i < N; i += 14){
+			for(int j = 0; j < M; j += 3){
+				for(int p = 0; p + i < N && p < 14; p ++){
+					// int tmp;
+					for(int q = 0; q + j < M && q < 3; q ++){
+						// if(i + p != q + j) 
+						B[q + j][p + i] = A[p + i][q + j];
+						// else tmp = A[p + i][q + j];
+					}
+					// if(i == j) B[p + j][p + i] = tmp;
+				}
+			}
+		}
 	}
 }
 
